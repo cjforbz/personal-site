708996f052c23652cb6f90d8514544ba987cb89f
Colin Forbes <56568946+cjforbz@users.noreply.github.com>
2022-04-29 17:50:43 -0400

    Navbar (#1)
    * coded out the skeleton of the navbar
    
    * coded out the skeleton of the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,1 +1,11 @@
-import React from 'react';
+const Navbar = () => {
+  return (
+    <div id="navbar">
+      <a>Home</a>
+      <a>About</a>
+      <a>Projects</a>
+    </div>
+  );
+};
+
+export default Navbar;
