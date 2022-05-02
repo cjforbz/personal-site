70bce8f5bc858b8b2490369c5259e0458911e590
Colin Forbes <cjforbes32@gmail.com>
2022-05-02 15:42:53 -0400

    built out skeleton of projects page, wrote descriptions/links/etc. for current projects

---
+++ public/projects.ts
@@ -0,0 +1,16 @@
+const projects = [
+  {
+    name: 'Bodega Swap',
+    description: `Bodega Swap is an app where users can trade their unwanted items for other users' items. It was built using React, Tailwind CSS, and Supabase, and deployed with Heroku. Bodega Swap is a PWA and is designed responsively to work on any screen size. Built as a final project for Fullstack Academy`,
+    github: 'https://github.com/FSA-Capstone-2201-Team-7/bodega-swap',
+    deployment: 'https://bodega-swap.herokuapp.com/',
+  },
+  {
+    name: 'Meme Supreme',
+    description: `Meme Supreme is an ecommerce website where users can go to buy the hottest, freshest memes. It was built using React, Express, Node, and PostgreSQL with Material UI, and Sequelize.`,
+    github: 'https://github.com/popplio-powersquad/graceshopper',
+    deployment: 'https://meme-supreme.herokuapp.com/',
+  },
+];
+
+export default projects;
