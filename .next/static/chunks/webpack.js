316f18b55520105422f5090e5ac815c6800c2b0c
Colin Forbes <cjforbes32@gmail.com>
2022-05-02 18:25:31 -0400

    added routing to navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "484a4001fec97c17"; }
+/******/ 		__webpack_require__.h = function() { return "5cd9ebbbb39325ae"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
