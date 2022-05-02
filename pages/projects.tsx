f0d1f88a992ea832e15d9ba19730818b5ca64cb7
Colin Forbes <cjforbes32@gmail.com>
2022-05-02 15:53:35 -0400

    Rendered the appropriate project info

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,20 @@
+import projects from '../public/projects';
 const Projects = () => {
-  return <div id="projects">Projects</div>;
+  return (
+    <div id="projects">
+      <h1>Projects</h1>
+      {projects.map((project, idx) => {
+        return (
+          <div className="project" key={idx}>
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+            <a href={project.github}>Github</a>
+            <a href={project.deployment}>Deployment</a>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Projects;
