cf012c39a071e450781d12fce8aa69710e3cd4ee
Colin Forbes <cjforbes32@gmail.com>
2022-05-09 18:33:58 -0400

    styling changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,11 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <div id="navbar">
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/projects">Projects</Link>
+      <div id="navbar-links">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/projects">Projects</Link>
+      </div>
     </div>
   );
 };
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,19 +1,21 @@
 import projects from '../public/projects';
 const Projects = () => {
   return (
-    <div id="projects">
+    <div>
       <h1>Projects</h1>
-      {projects.map((project, idx) => {
-        return (
-          <div className="project" key={idx}>
-            <h3>{project.name}</h3>
-            <img src={project.imgSrc} alt="" />
-            <p>{project.description}</p>
-            <a href={project.github}>Github</a>
-            <a href={project.deployment}>Deployment</a>
-          </div>
-        );
-      })}
+      <div id="projects">
+        {projects.map((project, idx) => {
+          return (
+            <div className="project" key={idx}>
+              <h3>{project.name}</h3>
+              <img src={project.imgSrc} alt="" />
+              <p>{project.description}</p>
+              <a href={project.github}>Github</a>
+              <a href={project.deployment}>Deployment</a>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
