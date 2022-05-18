2bd6f081665ddfd22f266592067c993f3abe6007
Colin Forbes <cjforbes32@gmail.com>
2022-05-18 17:57:03 -0400

    hamburger menu animated

--- components/Hamburger.tsx
+++ components/Hamburger.tsx
@@ -3,25 +3,32 @@ const Hamburger = () => {
   return (
     <div>
       <input type="checkbox" id="hamburger-input" className="burger-shower" />
-      <label id="hamburger-menu" htmlFor="hamburger-input">
-        <nav id="sidebar-menu">
-          <ul>
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-            <li>
-              <Link href="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
+      <label htmlFor="hamburger-input">
+        <div id="hamburger-menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </label>
-      <div className="overlay"></div>
+      <nav id="sidebar-menu">
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="overlay">
+        {/* <input type="checkbox" className="burger-shower" /> */}
+      </div>
     </div>
   );
 };
